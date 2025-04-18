6c30e23d48db35b70fb0be24f2fe67d26caf1afc
Georgiy <graubin1@mail.ru>
2025-04-18 16:56:49 +0300

    18.04.2025-16:56

--- windowworkers.cpp
+++ windowworkers.cpp
@@ -1,6 +1,9 @@
 #include "windowworkers.hpp"
 #include "ui_windowworkers.h"
 
+#include <QVBoxLayout>
+#include <iostream>
+
 WindowWorkers::WindowWorkers() : ui(new Ui::WindowWorkers)
 {
     ui->setupUi(this);
@@ -8,8 +11,32 @@ WindowWorkers::WindowWorkers() : ui(new Ui::WindowWorkers)
     setWindowTitle("ToolBox");
     setWindowIcon(QIcon(":/Images/mine.svg"));
     ui->lineEdit->setPlaceholderText("Поиск");
-
     setCardVisible(false);
+    db_ok = sqlite3_open("toolbox.db", &db);
+    sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS units (INTAGER PRIMARY KEY AUTOINKREMENT id,"
+                     " name TEXT)", nullptr, nullptr, &error_db);
+
+    //Добавление юнитов
+    add_unit = new QWidget(nullptr, Qt::WindowCloseButtonHint);
+    add_unit->setFixedWidth(300);
+    add_unit->setMaximumHeight(300);
+    add_unit->setMaximumHeight(600);
+    add_unit->setWindowTitle(" ");
+    add_unit->setWindowIcon(QIcon(":/Images/mine.svg"));
+    QVBoxLayout *add_unit_layout = new QVBoxLayout(add_unit);
+    add_unit_text = new QTextEdit(add_unit);
+    add_unit_text->setPlaceholderText("Введите ФИО юнита(-ов)");
+    QPushButton* add_unit_button = new QPushButton(add_unit);
+    add_unit_button->setText("Добавить");
+    add_unit_button->setStyleSheet("QPushButton { background-color: blue; border: none; "
+                                   "color: white; margin: 1px 1px; border-radius: 5px; } "
+                                   "QPushButton:hover { background-color: lightblue; } "
+                                   "QPushButton:pressed { background-color: #21618C; }");
+    add_unit_button->setFixedSize(100, 35);
+    connect(add_unit_button, &QPushButton::clicked, this, add_unit_get);
+    add_unit_layout->addWidget(add_unit_text);
+    add_unit_layout->addWidget(add_unit_button, 0, Qt::AlignHCenter);
+    add_unit->setLayout(add_unit_layout);
 }
 
 WindowWorkers::~WindowWorkers()
@@ -34,9 +61,29 @@ void WindowWorkers::setCardVisible(bool x)
     }
 }
 
+void WindowWorkers::add_unit_get()
+{
+    QString text = add_unit_text->toPlainText();
+
+}
+
+void WindowWorkers::pruf_db(int db_ok, char* error_db)
+{
+    if (db_ok) {
+        std::cerr << "Ошибка sqlite: " << error_db;
+    }
+}
+
 void WindowWorkers::on_action_3_triggered()
 {
-    setCardVisible(true);
+    if (!add_unit_show) {
+        add_unit->show();
+        add_unit_show = true;
+    }
+    else {
+        add_unit->hide();
+        add_unit->show();
+    }
 }
 
 
--- windowworkers.hpp
+++ windowworkers.hpp
@@ -2,6 +2,9 @@
 #define WINDOWWORKERS_HPP
 
 #include <QMainWindow>
+#include <QTextEdit>
+
+#include "sqlite3.h"
 
 namespace Ui {
 class WindowWorkers;
@@ -15,6 +18,10 @@ public:
     explicit WindowWorkers();
     ~WindowWorkers();
 
+    QWidget* add_unit = nullptr;
+    QTextEdit* add_unit_text = nullptr;
+    bool add_unit_show = false;
+
 private slots:
     void on_action_3_triggered();
 
@@ -23,8 +30,13 @@ private slots:
 private:
     Ui::WindowWorkers *ui;
     bool card_visible = false;
+    sqlite3* db;
+    int db_ok = 0;
+    char* error_db;
 
     void setCardVisible(bool);
+    void add_unit_get();
+    void pruf_db(int db_ok, char *error_db);
 };
 
 #endif // WINDOWWORKERS_HPP
